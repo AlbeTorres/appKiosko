ec288d1097064240daaff47af90d2ec513c821eb
AlbeTorres <albertocorreoficial@gmail.com>
2023-07-12 16:19:37 -0400

    fix: product endpoint mistakes

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,114 +1,115 @@
-const Producto = require('../models/Producto');
-const { validationResult} = require('express-validator');
-const cloudinary = require('cloudinary').v2
-
-
+const Producto = require("../models/Producto");
+const { validationResult } = require("express-validator");
+const cloudinary = require("cloudinary").v2;
 
 //crear un participante
-exports.crearProducto= async(req, res)=>{
-    
-    //revisar si hay errores
-    const errores = validationResult(req);
-    if(!errores.isEmpty()){
-        return res.status(400).json({errores: errores.array()});
-    }
+exports.crearProducto = async (req, res) => {
+  //revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
 
-    try {
-        //crear un nuevo producto
+  try {
+    //crear un nuevo producto
 
-        const producto= new Producto(req.body);
-        await producto.save();
-        res.status(200).json(producto);
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({msg:'Hubo un error'})
-        
-    }
-
-}
+    const producto = new Producto(req.body);
+    await producto.save();
+    res.status(200).json({producto});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Hubo un error" });
+  }
+};
 
 //obtener todos los productos
 
-exports.obtenerProductos= async(req, res)=>{
+exports.obtenerProductos = async (req, res) => {
+  try {
+    const productos = await Producto.find({});
 
-    try {
-        const productos = await Producto.find({});
-
-        res.status(200).json({productos});
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({msg:'Hubo un error'})
-        
-        
-    }
-}
+    res.status(200).json({ productos });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Hubo un error" });
+  }
+};
 
 //modificar un producto
 
-exports.modificarProducto= async(req, res)=>{
+exports.obtenerProducto = async (req, res) => {
+  try {
+    //comprobar que existe el producto
+    let producto = await Producto.findById(req.params.id);
 
-    try {
-
-        //comprobar que existe el producto
-        let productoFind = await Producto.findById(req.params.id);
-
-        if(!productoFind){
-            return res.status(404).json({msg:'Producto no encontrado'});
-        }
-
-        //actualizar producto
-        await Producto.updateOne({_id:req.params.id},{$set:req.body});
-        const producto = await Producto.findById(req.params.id)
-
-        res.status(200).json({producto});
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({msg:'Hubo un error'})
-        
+    if (!producto) {
+      return res.status(404).json({ msg: "Producto no encontrado" });
     }
 
-}
+    //devolver
+    res.status(200).json({ producto });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Hubo un error" });
+  }
+};
+exports.modificarProducto = async (req, res) => {
+  try {
+    //comprobar que existe el producto
+    let productoFind = await Producto.findById(req.params.id);
+
+    if (!productoFind) {
+      return res.status(404).json({ msg: "Producto no encontrado" });
+    }
+
+    //actualizar producto
+    await Producto.updateOne({ _id: req.params.id }, { $set: req.body });
+    const producto = await Producto.findById(req.params.id);
+
+    res.status(200).json({ producto });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Hubo un error" });
+  }
+};
 
 //eliminar un participante
-exports.eliminarProducto= async(req,res)=>{
-    cloudinary.config({
-        cloud_name: process.env.CLOUD_NAME,
-        api_key: process.env.APIKEY,
-        api_secret: process.env.APISECRET
-      });
+exports.eliminarProducto = async (req, res) => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.APIKEY,
+    api_secret: process.env.APISECRET,
+  });
 
-      console.log ({
-        cloud_name: process.env.CLOUD_NAME,
-        api_key: process.env.APIKEY,
-        api_secret: process.env.APISECRET
-      })
+  console.log({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.APIKEY,
+    api_secret: process.env.APISECRET,
+  });
 
-    try {
+  try {
+    //comprobar que existe el producto
+    let productoFind = await Producto.findById(req.params.id);
+    console.log(productoFind);
 
-        //comprobar que existe el producto
-        let productoFind = await Producto.findById(req.params.id);
-        console.log(productoFind)
-
-        if(!productoFind){
-            return res.status(404).json({msg:'Producto no encontrado'});
-        }
-
-        //eliminar
-
-        cloudinary.uploader.destroy(productoFind.cloud_id, function(error,result) {
-            console.log(result, error) });
-
-        await Producto.findOneAndRemove({_id:req.params.id});
-
-        res.status(200).json({msg:'Producto eliminado correctamente'});
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({msg:'Hubo un error'})
+    if (!productoFind) {
+      return res.status(404).json({ msg: "Producto no encontrado" });
     }
 
+    //eliminar
 
-}
+    cloudinary.uploader.destroy(
+      productoFind.cloud_id,
+      function (error, result) {
+        console.log(result, error);
+      }
+    );
+
+    await Producto.findOneAndRemove({ _id: req.params.id });
+
+    res.status(200).json({ msg: "Producto eliminado correctamente" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Hubo un error" });
+  }
+};
--- routes/productos.js
+++ routes/productos.js
@@ -1,37 +1,35 @@
 // rutas productos
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productoController = require('../controllers/productoController');
-const {check} = require('express-validator');
-const auth = require('../middleware/auth');
-
+const productoController = require("../controllers/productoController");
+const { check } = require("express-validator");
+const auth = require("../middleware/auth");
 
 //crear un producto
 //api/productos
-router.post('/',auth,
-    [ 
-        check('nombre','El nombre es obligatorio').not().isEmpty(),
-        check('medida', 'La medida es obligatoria').not().isEmpty(),
-        check('img', 'La imagen es obligatoria').not().isEmpty(),
-    
-    ],
-    productoController.crearProducto
+router.post(
+  "/",
+  auth,
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("medida", "La medida es obligatoria").not().isEmpty(),
+    check("img", "La imagen es obligatoria").not().isEmpty(),
+  ],
+  productoController.crearProducto
 );
 
 //obtener todos los participantes
-router.get('/',
-    productoController.obtenerProductos
-    );
+router.get("/", productoController.obtenerProductos);
+
+
+/* `router.get("/:id", productoController.obtenerProducto);` is defining a GET route for retrieving a
+specific product by its ID. */
+router.get("/:id", productoController.obtenerProducto);
 
 //modificar un participante
-router.patch('/:id',auth,
-    productoController.modificarProducto
-    );
+router.patch("/:id", auth, productoController.modificarProducto);
 
 //eliminar un participante
-router.delete('/:id',auth,
-    productoController.eliminarProducto
-    );
+router.delete("/:id", auth, productoController.eliminarProducto);
 
-
-module.exports= router;
+module.exports = router;
