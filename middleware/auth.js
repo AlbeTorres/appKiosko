bd7e07b43ba5ae21e21eb6e811d673040b0c2a96
AlbeTorres <albertocorreoficial@gmail.com>
2023-07-17 16:58:21 -0400

    fix: error on middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,15 +3,15 @@ const jwt = require('jsonwebtoken');
 module.exports = function(req, res, next){
     //leer el token del head
     const token = req.header('x-auth-token');
-    const tokenObject = JSON.parse(token, 'base64');
-
+    
     //revisar si no hay token
     if(!token){
         return res.status(401).json({msg:'No hay token, permiso no válido'});
     }
     //validar el token
     try {
-
+        
+        const tokenObject = JSON.parse(token, 'base64');
         const cifrado = jwt.verify(tokenObject,process.env.SECRETA);
         req.usuario = cifrado.usuario;
         next();
