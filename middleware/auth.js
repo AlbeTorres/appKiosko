7466c1e7a98f12b62fd85be5c21a659ab06c45db
AlbeTorres <albertocorreoficial@gmail.com>
2023-07-17 13:10:43 -0400

    feat: parse string auth token to json

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,20 +3,21 @@ const jwt = require('jsonwebtoken');
 module.exports = function(req, res, next){
     //leer el token del head
     const token = req.header('x-auth-token');
+    const tokenObject = JSON.parse(token, 'base64');
 
     //revisar si no hay token
     if(!token){
         return res.status(401).json({msg:'No hay token, permiso no válido'});
     }
-
     //validar el token
     try {
-        
-        const cifrado = jwt.verify(token,process.env.SECRETA);
+
+        const cifrado = jwt.verify(tokenObject,process.env.SECRETA);
         req.usuario = cifrado.usuario;
         next();
 
     } catch (error) {
+        console.log(error)
         res.status(401).json({msg:'token no válido'});
     }
 }
