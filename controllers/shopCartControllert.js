3cca618e2aa611b53230da5d3e0349aaa7fc1a8c
AlbeTorres <albertocorreoficial@gmail.com>
2023-07-25 14:29:41 -0400

    feat: add getcartproduct endpoint

--- controllers/shopCartControllert.js
+++ controllers/shopCartControllert.js
@@ -7,6 +7,12 @@ const getProductPrice = async (id) => {
   return product.precio;
 };
 
+const getProduct = async (id) => {
+  const product = await Producto.findById(id);
+
+  return product;
+};
+
 exports.getCartPrice = async (req, res) => {
   const errores = validationResult(req);
   if (!errores.isEmpty()) {
@@ -24,6 +30,33 @@ exports.getCartPrice = async (req, res) => {
     }, 0);
 
     res.status(200).json({ totalCartPrice });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Hubo un error" });
+  }
+};
+
+exports.getCartProducts = async (req, res) => {
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
+  try {
+    const cartproductsid = JSON.parse(req.query.cartproducts);
+
+    const cartproducts = await cartproductsid.reduce(
+      async (cartproducts, p) => {
+        let aux = await getProduct(p._id);
+
+        return [...cartproducts, aux];
+      },
+      []
+    );
+
+    console.log(cartproducts);
+
+    res.status(200).json({ cartproducts });
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "Hubo un error" });
--- routes/shopcart.js
+++ routes/shopcart.js
@@ -2,15 +2,26 @@
 const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
-const shopCartController= require("../controllers/shopCartControllert")
-
+const shopCartController = require("../controllers/shopCartControllert");
 
 router.get(
-    "/",
-    [
-      check("cartproducts", "El array de cartproducts es obligatorio").not().isEmpty(),
-    ],
-    shopCartController.getCartPrice
-  );
+  "/",
+  [
+    check("cartproducts", "El array de cartproducts es obligatorio")
+      .not()
+      .isEmpty(),
+  ],
+  shopCartController.getCartPrice
+);
 
-  module.exports = router;
+router.get(
+  "/products",
+  [
+    check("cartproducts", "El array de cartproducts es obligatorio")
+      .not()
+      .isEmpty(),
+  ],
+  shopCartController.getCartProducts
+);
+
+module.exports = router;
