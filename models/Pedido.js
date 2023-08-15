5915b5a45268bdbcd2600203a4d378854238889b
AlbeTorres <albertocorreoficial@gmail.com>
2023-08-15 14:08:24 -0400

    feat: change pedido model

--- models/Pedido.js
+++ models/Pedido.js
@@ -1,48 +1,74 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const PedidoSchema = mongoose.Schema({
-
-    usuario:{
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref:'Usuario'
-    },
-    productos:{
-        type: Array,
-        required: true,
-    },
-    estado:{
-        type: String,
-        required: true,
-    },
-    direccion:{
-        type: String,
-        required: true,
-    },
-    receptor:{
-        type: String,
-        required: true,
-    },
-    movil:{
-        type: String,
-        required: true,
-    },
-    pago:{
-        type: String,
-        required: true,
-    },
-    valor:{
-        type: Number,
-        required: true,
-    },
-    fechaini:{
-        type: Date,
-        default: Date.now()
-    },
-    fechafin:{
-        type:Date
-    }
-
+  usuario: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Usuario",
+  },
+  productos: {
+    type: Array,
+    required: true,
+  },
+  estado: {
+    type: String,
+    required: true,
+  },
+  direccion: {
+    type: String,
+    required: true,
+  },
+  provincia: {
+    type: String,
+    required: true,
+  },
+  municipio: {
+    type: String,
+    required: true,
+  },
+  receptor: {
+    type: String,
+    required: true,
+  },
+  zipcode: {
+    type: Number,
+    required: true,
+  },
+  carnet: {
+    type: Number,
+    required: true,
+  },
+  movil: {
+    type: String,
+    required: true,
+  },
+  metodo_pago: {
+    type: String,
+    required: true,
+  },
+  valor_total: {
+    type: Number,
+    required: true,
+  },
+  valor_subtotal: {
+    type: Number,
+    required: true,
+  },
+  valor_descuentos: {
+    type: Number,
+    required: true,
+  },
+  valor_envio: {
+    type: Number,
+    required: true,
+  },
+  fechaini: {
+    type: Date,
+    default: Date.now(),
+  },
+  fechafin: {
+    type: Date,
+  },
 });
 
-module.exports = mongoose.model('Pedido', PedidoSchema);
+module.exports = mongoose.model("Pedido", PedidoSchema);
